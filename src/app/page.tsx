d1e233d1c503ce67c118da20d83103b4b8af76d0
Daniel Núñez <daniel.portonunez@gmail..com>
2024-03-27 15:27:19 -0300

    feat: add home page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,76 @@
 import { Editor } from '@/components'
+import Link from 'next/link'
 
 export default function Page() {
   return (
-    <Editor.CodeSide fullSize className="col-span-1 flex">
-      <h1>Page</h1>
+    <Editor.CodeSide
+      fullSize
+      className="col-span-1 flex items-center justify-center relative"
+    >
+      <div className="flex flex-col gap-4">
+        <span>Hi all, I&apos;m</span>
+        <h1 className="font-extrabold text-6xl">
+          <span>Daniel Núñez</span>
+        </h1>
+        <h2 className="text-lg">
+          <span>{'> '}</span>
+          <span>
+            Full Stack Engineer, specialized in Javascript environment
+          </span>
+        </h2>
+
+        <span>
+          {'// '} fell free to access the code of this page in my github
+          <span className="block">
+            <span>const url = </span>
+            <Link
+              className="text-yellow-500"
+              target="_blank"
+              href="https://github.com/dpnunez/portfolio"
+            >
+              {'"'}https://github.com/dpnunez/portfolio{'"'}
+            </Link>
+          </span>
+        </span>
+        <ComputerBackground />
+      </div>
     </Editor.CodeSide>
+  )
+}
+
+function ComputerBackground() {
+  return (
+    <pre className="absolute text-2xl top-0 left-0 opacity-5 pointer-events-none">
+      {`.,,uod8B8bou,,.
+              ..,uod8BBBBBBBBBBBBBBBBRPFT?l!i:.
+         ,=m8BBBBBBBBBBBBBBBRPFT?!||||||||||||||
+         !...:!TVBBBRPFT||||||||||!!^^""'   ||||
+         !.......:!?|||||!!^^""'            ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         !.........||||                     ||||
+         '.........||||                    ,||||
+          .;.......||||               _.-!!|||||
+   .,uodWBBBBb.....||||       _.-!!|||||||||!:'
+!YBBBBBBBBBBBBBBb..!|||:..-!!|||||||!iof68BBBBBb....
+!..YBBBBBBBBBBBBBBb!!||||||||!iof68BBBBBBRPFT?!::   '.
+!....YBBBBBBBBBBBBBBbaaitf68BBBBBBRPFT?!:::::::::     '.
+!......YBBBBBBBBBBBBBBBBBBBRPFT?!::::::;:!^"';:::       '.
+!........YBBBBBBBBBBRPFT?!::::::::::^''...::::::;         iBBbo.
+'..........YBRPFT?!::::::::::::::::::::::::;iof68bo.      WBBBBbo.
+  '..........:::::::::::::::::::::::;iof688888888888b.     'YBBBP^'
+    '........::::::::::::::::;iof688888888888888888888b.     '
+      '......:::::::::;iof688888888888888888888888888888b.
+        '....:::;iof688888888888888888888888888888888899fT!
+          '..::!8888888888888888888888888888888899fT|!^"'
+            '' !!988888888888888888888888899fT|!^"'
+                '!!8888888888888888899fT|!^"'
+                  '!988888888899fT|!^"'
+                    '!9899fT|!^"'
+                      '!^"'`}
+    </pre>
   )
 }
