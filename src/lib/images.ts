e637de15c42d8e7beadb1915f34525477c3763c8
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-05 21:36:21 -0300

    feat: use sharp instead of plaiceholder

--- src/lib/images.ts
+++ src/lib/images.ts
@@ -1,10 +1,21 @@
 import { promises as fs } from 'fs'
 import path from 'path'
-import { getPlaiceholder } from 'plaiceholder'
+import sharp from 'sharp'
 
 interface GeneratePlaceholder {
   base64?: string
   src: string
+}
+
+function bufferToBase64(buffer: Buffer): string {
+  return `data:image/png;base64,${buffer.toString('base64')}`
+}
+
+async function getBufferFromFilePath(file: string): Promise<Buffer> {
+  const pathFile = path.join(process.cwd(), 'public', file)
+  const buffer = await fs.readFile(pathFile)
+
+  return buffer
 }
 
 export async function generatePlaceholder(
@@ -12,12 +23,12 @@ export async function generatePlaceholder(
 ): Promise<GeneratePlaceholder> {
   'use server'
   try {
-    const pathFile = path.join(process.cwd(), 'public', file)
-    const buffer = await fs.readFile(pathFile)
-    const plaiceholder = await getPlaiceholder(buffer)
+    const buffer = await getBufferFromFilePath(file)
+    const resizedBuff = await sharp(buffer).resize(20).toBuffer()
+    const base64 = bufferToBase64(resizedBuff)
 
     return {
-      ...plaiceholder,
+      base64,
       src: file,
     }
   } catch {
