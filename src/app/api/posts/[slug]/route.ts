e3143bb193f7a815e78a1eb06f14c3f010ae23d3
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-07 15:59:59 -0300

    fix: remove useless dev.to api key

--- src/app/api/posts/[slug]/route.ts
+++ src/app/api/posts/[slug]/route.ts
@@ -15,11 +15,6 @@ export async function GET(
 
   const data = await ky(
     `https://dev.to/api/articles/${process.env.DEV_TO_USERNAME}/${slug}`,
-    {
-      headers: {
-        'api-key': process.env.DEV_TO_API_KEY,
-      },
-    },
   ).json<PostWithContent>()
 
   return NextResponse.json({
