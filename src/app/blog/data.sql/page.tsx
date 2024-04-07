bd6c9399c0ed3e219ef5b4ed1428c62b15f34734
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-07 16:17:57 -0300

    feat: add empty state

--- src/app/blog/data.sql/page.tsx
+++ src/app/blog/data.sql/page.tsx
@@ -4,12 +4,21 @@ import { api } from '@/lib/api'
 export default async function Page() {
   const { data } = await api.get(`api/posts`).json<ResponseApi<Post[]>>()
 
+  if (data.length === 0) {
+    return (
+      <h1 className="text-4xl italic font-bold text-pink-500 opacity-50 col-span-4 w-full items-center justify-center flex flex-col">
+        No posts found
+      </h1>
+    )
+  }
+
   return data.map((post) => (
-    <Card
-      href={`/blog/${post.slug}`}
-      key={post.slug}
-      image={post.cover_image}
-      secondaryTitle={post.title}
-    />
+    <div className="col-span-1" key={post.slug}>
+      <Card
+        href={`/blog/${post.slug}`}
+        image={post.cover_image}
+        secondaryTitle={post.title}
+      />
+    </div>
   ))
 }
