d4fcab66689d59bc4751e30f00cc54eb2a7606f6
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-07 16:12:22 -0300

    feat: add mobile layout

--- src/app/blog/data.sql/loading.tsx
+++ src/app/blog/data.sql/loading.tsx
@@ -1,19 +1,23 @@
-import { Skeleton } from '@/components'
+import { Card, Skeleton } from '@/components'
 import { generateRandomWidth } from '@/lib/utils'
 
 export default function Loading() {
   return Array.from({
     length: 20,
   }).map((_, i) => (
-    <div key={i} className="flex flex-col gap-2">
-      <Skeleton className="w-full aspect-video rounded-xl mb-2" />
-      <Skeleton className="h-5 w-full rounded-full animate-pulse" />
-      <Skeleton
-        style={{
-          width: generateRandomWidth(10, 96) + '%',
-        }}
-        className="h-5 rounded-full animate-pulse"
-      />
-    </div>
+    <Card
+      key={i}
+      secondaryTitle={
+        <div>
+          <Skeleton className="h-5 w-full rounded-full animate-pulse mb-2" />
+          <Skeleton
+            style={{
+              width: generateRandomWidth(10, 96) + '%',
+            }}
+            className="h-5 rounded-full animate-pulse"
+          />
+        </div>
+      }
+    />
   ))
 }
