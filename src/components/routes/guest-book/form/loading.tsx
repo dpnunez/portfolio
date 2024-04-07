f76559519f43d4ecb4aa1cc93756f5172dd65ada
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-07 00:11:38 -0300

    feat: add sql loader on guest page

--- src/components/routes/guest-book/form/loading.tsx
+++ src/components/routes/guest-book/form/loading.tsx
@@ -1,5 +1,12 @@
-import { Skeleton } from '@/components'
+import { CodeSnippet, Spinner } from '@/components'
 
 export function Loader() {
-  return <Skeleton className="h-10 w-full flex-1" />
+  return (
+    <div className="flex items-center gap-4">
+      <Spinner />
+      <CodeSnippet showLineNumbers={false} language="sql">
+        SELECT username, email, createdAt FROM Guest
+      </CodeSnippet>
+    </div>
+  )
 }
