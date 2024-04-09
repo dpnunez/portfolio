c91892cd0340ada3c6835adf255f2458a56ee7a3
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-09 18:15:59 -0300

    feat(home): add enter content animation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,26 +10,28 @@ export default function Page() {
       fullSize
       className="col-span-1 flex items-center justify-center relative max-w-full overflow-hidden"
     >
-      <div className="flex flex-col gap-4 items-center max-w-full p-4 md:items-start">
-        <span>Hi all, I&apos;m</span>
-        <h1 className="font-extrabold text-3xl md:text-6xl">
-          <span>Daniel Núñez</span>
-        </h1>
-        <h2 className="text-lg mb-4">
-          <span>{'> '}</span>
-          <span>
-            Full Stack Engineer, specialized in Javascript environment
-          </span>
-        </h2>
+      <div className="flex flex-col items-center max-w-full p-4 md:items-start">
+        <Editor.EnterCode className="gap-4 flex flex-col">
+          <span>Hi all, I&apos;m</span>
+          <h1 className="font-extrabold text-3xl md:text-6xl">
+            <span>Daniel Núñez</span>
+          </h1>
+          <h2 className="text-lg mb-4">
+            <span>{'> '}</span>
+            <span>
+              Full Stack Engineer, specialized in Javascript environment
+            </span>
+          </h2>
 
-        <Link target="_blank" href={repoLink}>
-          <PrismLight
-            className="!bg-transparent hidden md:block"
-            language="javascript"
-          >
-            {`// fell free to access the code of this page in my github\nconst url = "${repoLink}"`}
-          </PrismLight>
-        </Link>
+          <Link target="_blank" href={repoLink}>
+            <PrismLight
+              className="!bg-transparent hidden md:block"
+              language="javascript"
+            >
+              {`// fell free to access the code of this page in my github\nconst url = "${repoLink}"`}
+            </PrismLight>
+          </Link>
+        </Editor.EnterCode>
         <ASCII className="absolute top-0 left-0 opacity-5 max-w-full max-h-full">
           {computerAscii}
         </ASCII>
