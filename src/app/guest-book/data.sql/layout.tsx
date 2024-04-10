67236b8f4c188768b4559f8cbac8a71fe659a233
Daniel Núñez <daniel.portonunez@gmail..com>
2024-04-10 19:32:09 -0300

    fix: add custom authOptions on serverside

--- src/app/guest-book/data.sql/layout.tsx
+++ src/app/guest-book/data.sql/layout.tsx
@@ -1,6 +1,7 @@
 import { NextAuthProvider } from '@/providers/nextauth-provider'
 import { getServerSession } from 'next-auth'
 import type { Metadata } from 'next'
+import { authOptions } from '@/lib/nextAuth'
 
 export const metadata: Metadata = {
   title: '~/guest-book/data.sql',
@@ -11,6 +12,7 @@ export default async function Layout({
 }: {
   children: React.ReactNode
 }) {
-  const session = await getServerSession()
+  const session = await getServerSession(authOptions)
+
   return <NextAuthProvider session={session}>{children}</NextAuthProvider>
 }
